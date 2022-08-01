84b9bcd64b36e761d8d1026c8097761a67740043
Eric Melonakos <eric.melonakos@gmail.com>
2022-08-01 16:26:28 -0400

    change filtering method

--- misc_Functions.py
+++ misc_Functions.py
@@ -623,7 +623,7 @@ def threshFunc(dataArray, threshVal):
     return dataArray
 
 
-def filterData(t, data, n="", wn="", channel='CBvsPFCEEG', ftype="", btype=""):
+def filterData(t, data, n=5, wn=[0.5,4], ftype='Butterworth', btype='band'):
     """ Use ftype to indicate FIR or Butterworth filter.
     
     For the FIR filter indicate a LowPass, HighPass, or BandPass with btype = lowpass, highpass, or bandpass . 
@@ -632,21 +632,21 @@ def filterData(t, data, n="", wn="", channel='CBvsPFCEEG', ftype="", btype=""):
     Channel should be set to desired .ncs file
     
     The Butterworth filters have a more linear phase response in the pass-band than other types and is able to provide better group delay performance, and also a lower level of overshoot.
-    Indicate the filter type by setting ftype = low, high, or band.
+    Indicate the filter type by setting btype = low, high, or band.
     To set the cutoff frequency use wn= 
-    The default for n is n =5
+    The default for n is n = 5
     For a bandpass filter indicate the lowstop and the highstop by using an array. example: wn= ([10, 30])"""
 
-    dt = t[channel][1] - t[channel][0]  # Define the sampling interval.
+    dt = t[1] - t[0]  # Define the sampling interval.
     fNQ = 1 / dt / 2  # Determine the Nyquist frequency.
     cut = wn / fNQ  # ... and specify the cutoff frequency,
 
     if ftype == "FIR":
-        b, a = firwin(n, cut, btype)  # ... build bandpass FIR filter,
+        b, a = firwin(n, cut, pass_zero=btype)  # ... build bandpass FIR filter,
         filteredData = filtfilt(b, a, data)  # ... and zero-phase filter each trial
 
     if ftype == "Butterworth":
-        b, a = signal.butter(n, cut, btype)
+        b, a = signal.butter(n, cut, btype=btype)
         filteredData = signal.filtfilt(b, a, data)
 
     return filteredData
