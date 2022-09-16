85792665e416f98b10ee7d35e0e166d6b2ea1bbd
Eric Melonakos <eric.melonakos@gmail.com>
2022-09-16 13:05:44 -0400

    fix miniscope class name

--- visualize_estimates_data.py
+++ visualize_estimates_data.py
@@ -10,13 +10,13 @@ For examining the spatial and temporal components of a calcium imaging movie.
 import miniscope
 import caiman as cm
 
-obj = miniscope.miniscope(lineNum=16)
+obj = miniscope.UCLAMiniscope(lineNum=16)
 
 # %% Import videos
-obj.importCaMovies('cluster_testing/cropping_samples/Yr0000_d1_390_d2_388_d3_1_order_C_frames_1000_.mmap')
+obj.importCaMovies('cluster_testing/multiple_movies/same_analysis_twice/Yr_d1_390_d2_388_d3_1_order_C_frames_3000_.mmap')
 
 # %% Load the estimates object
-cnmObj = cm.source_extraction.cnmf.cnmf.load_CNMF('cluster_testing/cropping_samples/estimates.hdf5')
+cnmObj = cm.source_extraction.cnmf.cnmf.load_CNMF('cluster_testing/multiple_movies/same_analysis_twice/estimates.hdf5')
 
 obj.estimates = cnmObj.estimates
 
