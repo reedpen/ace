656f80bf91fab91f7d01dbdc3f45b71aba588b08
Eric Melonakos <eric.melonakos@gmail.com>
2022-09-26 13:00:32 -0400

    make experiment_analysis a package

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -6,7 +6,7 @@ Created on Fri Aug 14 11:25:19 2020
 
 """
 
-# import os
+import os
 os.chdir('..')
 # import glob
 
@@ -32,16 +32,16 @@ obj = miniscope.UCLAMiniscope(lineNum=16, jobID=jobID)
 #     obj.preprocessCaMovies(saveMovie=True, crop=True, cropGUI=False)
 
 # %% Import videos
-obj.importCaMovies(['../../experimental_data/miniscope_data/test/R220606/2022_07_21/14_40_42/Miniscope/0_cropped.avi'])#, '../../experimental_data/miniscope_data/test/R220606/2022_07_21/14_40_42/Miniscope/1_cropped.avi', '../../experimental_data/miniscope_data/test/R220606/2022_07_21/14_40_42/Miniscope/2_cropped.avi'])
+#obj.importCaMovies(['../../experimental_data/miniscope_data/test/R220606/2022_07_21/14_40_42/Miniscope/0_cropped.avi'])#, '../../experimental_data/miniscope_data/test/R220606/2022_07_21/14_40_42/Miniscope/1_cropped.avi', '../../experimental_data/miniscope_data/test/R220606/2022_07_21/14_40_42/Miniscope/2_cropped.avi'])
 # obj.importCaMovies(['D:/Dropbox/Documents/Brown_Lab/experimental_data/miniscope_data/test/R220606/2022_07_21/14_40_42/Miniscope/0.avi','D:/Dropbox/Documents/Brown_Lab/experimental_data/miniscope_data/test/R220606/2022_07_21/14_40_42/Miniscope/90.avi'])
 # obj.importCaMovies(['D:/Dropbox/Documents/Brown_Lab/experimental_data/miniscope_data/test/R220606/2022_07_21/14_40_42/Miniscope/memmap__d1_390_d2_388_d3_1_order_C_frames_1000_.mmap'])
 
 # %% Run motion correction and CNMF-E and save the estimates object
-obj.processCaMovies(parallel=False, n_processes=8)
+#obj.processCaMovies(parallel=False, n_processes=8)
 
 # %% Plot spectrogram
 # obj.computeSpectrogram(plotSpectrogram=True, plotEvents=False)
-print('obj.movieFilePaths = ' + str(obj.movieFilePaths))
+#print('obj.movieFilePaths = ' + str(obj.movieFilePaths))
 
 # %% Delete memmapped files
 # # Get a list of all the file paths that ends with .txt from in specified directory
