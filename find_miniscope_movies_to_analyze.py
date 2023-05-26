6dfada85ea3dbfa87495b562d3b47484b0fcac73
Eric Melonakos <eric.melonakos@gmail.com>
2023-05-26 17:02:38 -0400

    improve method to find miniscope movies

--- find_miniscope_movies_to_analyze.py
+++ find_miniscope_movies_to_analyze.py
@@ -9,21 +9,39 @@ import miniscope_ephys
 import numpy as np
 
 channel = 'PCvsPFCEEG'
+startFreq = 0.5
+endFreq = 4.0
 
-for k in [35, 42, 48, 37, 39, 41, 38, 45, 40]:
-    obj = miniscope_ephys.miniscopeEphys(k)
+obj = []
+
+for q, k in enumerate([35, 42, 48, 37, 39, 41, 38, 45, 40]):
+#%% Load experiment and ephys data
+    obj.append(miniscope_ephys.miniscopeEphys(k))
     
-    obj.importEphysData(channels=channel)
-    # obj.importNeuralynxEvents(analogSignalImported=False)
+    obj[q].importEphysData(channels=channel)
+    # obj[q].importNeuralynxEvents(analogSignalImported=False)
     
-    h, ax = obj.computeSpectrogram(channel=channel, plotSpectrogram=True, plotEvents=False, windowLength=10)
-    ax.set_title(obj.experiment['line number'] + ': ' + obj.experiment['id'] + ', ' + obj.experiment['systemic drug'])
     
-    # obj.syncNeuralynxMiniscopeTimestamps(channel=channel)
-    # obj.findEphysIdxOfTTLEvents(CaEvents=False)
+#%% Compute and plot the spectrograms
+    h, ax = obj[q].computeSpectrogram(channel=channel, plotSpectrogram=True, plotEvents=False, windowLength=10)
+    ax.set_title(obj[q].experiment['line number'] + ': ' + obj[q].experiment['id'] + ', ' + obj[q].experiment['systemic drug'])
     
-    # a = obj.tEphys[channel][obj.ephysIdxAllTTLEvents]-obj.tEphys[channel][obj.ephysIdxAllTTLEvents][0]
     
-    # timeSec = 3600
+#%% Find high slow wave power
+    # After you have looked at the spectrograms and found areas that you think have high slow wave power, run the next cell to plot the mean power in the slow wave band
+    bandStart = np.where(obj[q].freqsSpect>=startFreq)[0]
+    bandEnd = np.where(obj[q].freqSpect<=endFreq)[0]
+    meanBand = np.mean(obj[q].pSpect[bandStart:bandEnd+1,:],axis=0)
     
-    # t = np.where(a>timeSec)[0]
+    
+#%% Find miniscope indices of high slow wave power
+    timeSecStart = 5861
+    timeSecEnd = 6251
+    
+    obj[q].syncNeuralynxMiniscopeTimestamps(channel=channel)
+    obj[q].findEphysIdxOfTTLEvents(CaEvents=False)
+    
+    a = obj[q].tEphys[channel][obj[q].ephysIdxAllTTLEvents]-obj[q].tEphys[channel][obj[q].ephysIdxAllTTLEvents][0]
+    
+    tStart = np.where(a>timeSecStart)[0]
+    tEnd = np.where(a<timeSecEnd)[0]
