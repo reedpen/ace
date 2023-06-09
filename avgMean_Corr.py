79fa5c0ddb8b368b4da2fde6eb625dd1f1563c45
rpfisher9 <133471152+rpfisher9@users.noreply.github.com>
2023-06-09 09:09:52 -0400

    Add files via upload
    Adding new file for when I write the average frame fluorescence and correlation to original script

---
+++ avgMean_Corr.py
@@ -0,0 +1,24 @@
+### testing
+"""
+Created on Fri June 9 9:00:00 2023
+
+@author: Rachael Fisher 
+
+This script is used to crop within lens and find average fluorescence, stats for correlation
+"""
+import miniscope 
+
+channel = 'PFCEEGvsCBEEG'
+
+
+obj = miniscope_ephys.miniscopeEphys(lineNum=lineNum)
+
+obj.importEphysData(channels=channel)
+obj.importNeuralynxEvents(analogSignalImported=True)
+obj.syncNeuralynxMiniscopeTimestamps(channel=channel)
+
+obj.findEphysIdxOfTTLEvents(channel=channel, CaEvents=False)
+
+obj.filterEphys(channel=channel)
+
+obj.ephys[channel][obj.ephysIdxAllTTLEvents]
