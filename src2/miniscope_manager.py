180c48776a95bec59e99e85d9032091a0151ca4c
noah08642 <lukerichards8@gmail.com>
2025-03-14 13:25:44 -0600

    In progress writing miniscope_processor.

---
+++ src2/miniscope_manager.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  2 11:20:05 2025
+ 
+@author: lukerichards
+"""
+
+from src2.miniscope_data_manager import MiniscopeDataManager
+from src2.miniscope_processor import MiniscopeProcessor
+from src2.visualizer import Visualizer
+from typing import List
+
+
+
+class MiniscopeWorkflow:
+    """Main workflow class."""
+
+    def __init__(self):
+        pass
+    
+    def run(
+            self, 
+            line_num,
+            channel_name = 'PFCLFPvsCBEEG',
+            remove_artifacts = False,
+            filter_type = None, # if desired, enter the type, eg "butter"
+            filter_range = [0.5, 4],
+            plot_channel = False,
+            plot_spectrogram = False,
+            logging_level = "CRITICAL"
+            ):
+        
+
+        dm = MiniscopeDataManager()
+        p = MinicopeProcessor(dm.)
+
+        # this is tough.  
+
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ src2/projections.py
@@ -0,0 +1,11 @@
+
+
+class Projections():
+    def __init__(self, max: float, std: float, min: float, mean: float, median: float, range: float, time: float):
+        self.max = max
+        self.std = std
+        self.min = min
+        self.mean = mean
+        self.median = median
+        self.range = range
+        self.time = time
