9c9c84fe3c91b95692516f6c60544eeef8ecd398
aathok-git <aathokmail@gmail.com>
2025-10-10 16:40:36 -0600

    Updating the credential instructions as I learn more about box

--- data/BLANK_box_credentials.py
+++ data/BLANK_box_credentials.py
@@ -1,8 +1,11 @@
 """This is a file to safely store your box credentials and keep them secure during your git commits.
+THIS PROGRAM WAS DEVELOPED FOR BOX-SDK-10.0.1
 
 To properly set up this file:
 1. Make a copy of this file and rename it to "box_credentials.py" 
 2. Insert your box developer token in the field labled PUT_YOUR_BOX_DEVELOPER_TOKEN_HERE
+    a. To obtain your box developer token, go to the box developer console at <https://byu.app.box.com/developers/console>
+    b. create a custom app and select "Server Authentication (Client Credentials Grant)" 
 3. You're done!"""
 
 dev_token = 'PUT_YOUR_BOX_DEVELOPER_TOKEN_HERE'
