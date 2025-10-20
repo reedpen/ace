00f67856bba06c7064088673425c2e966bd7a03c
aathok-git <aathokmail@gmail.com>
2025-10-20 12:22:00 -0600

    modularized auth w/ BoxDeveloperToken, filedownloader works. Next Step: Develop permanant authorization, add command line compatibility

--- data/file_downloader.py
+++ data/file_downloader.py
@@ -1,5 +1,5 @@
-from box_credentials import dev_token, base_file_path
-from box_sdk_gen import BoxClient, BoxDeveloperTokenAuth
+from box_credentials import dev_token, BASE_FILE_PATH
+from box_sdk_gen import BoxClient, BoxDeveloperTokenAuth   
 from os import path as os_path, makedirs
 
 """TO DO:
@@ -8,26 +8,24 @@ from os import path as os_path, makedirs
     - Work with teammates to develop standardized nomenclature and use of this file.
 """
         
-auth = None
 client = None
     
 def make_auth():
-    BoxDeveloperTokenAuth = BoxDeveloperTokenAuth(token=dev_token)
-    auth = BoxDeveloperTokenAuth
-    BoxClient = BoxClient(auth=auth)
-    client = BoxClient
-
+    auth = BoxDeveloperTokenAuth(token=dev_token)
+    client = BoxClient(auth=auth)
+    return client
+    
 def verify_file(path: str):
     """Checks the path where the file should be.
     If a folder doesn't exist, break the search and call download_file(path) to download and store the file
     """
     if not client:
-        makeauth()
-    if not os_path.exists(f"{base_file_path}/{path}"):
-        makedirs(f"{base_file_path}/{path}")
+        client = make_auth()
+    if not os_path.exists(f"{BASE_FILE_PATH}/{path}"):
+        makedirs(f"{BASE_FILE_PATH}/{path}")
         file_id = get_file_id(path)
         download_file(path, file_id)
-    elif os_path.exists(f"{base_file_path}/{path}"):
+    elif os_path.exists(f"{BASE_FILE_PATH}/{path}"):
         return True
     else:
         print("Error (error message here)")
@@ -44,20 +42,20 @@ def download_file(path: str, ID):
     """Called from verify_file or run manually to download a file from box and store it in a standard path
     """
     if not client:
-        make_auth()
+        client = make_auth()
     
     try:
         for item in client.folders.get_folder_items(ID).entries:
             print(item.name)
             if item.type == 'folder':
                 print(f"Found a folder: {item.name}")
-                makedirs(f"{base_file_path}/{path}/{item.name}")
+                makedirs(f"{BASE_FILE_PATH}/{path}/{item.name}")
                 download_file(path=f"{path}/{item.name}", ID=item.id)
                 continue
         
-            with open(f"{base_file_path}/{path}/{item.name}", "wb") as output_file:
+            with open(f"{BASE_FILE_PATH}/{path}/{item.name}", "wb") as output_file:
                 client.downloads.download_file_to_output_stream(item.id, output_stream=output_file)
-                print(f"File '{item.name}' downloaded successfully to '{base_file_path}/{path}/{item.name}'")
+                print(f"File '{item.name}' downloaded successfully to '{BASE_FILE_PATH}/{path}/{item.name}'")
 
         return True # Returns True once everthing is downloaded
     except Exception as e:
@@ -66,6 +64,5 @@ def download_file(path: str, ID):
 
 
 if __name__ == '__main__':
-    auth: BoxDeveloperTokenAuth = BoxDeveloperTokenAuth(token=dev_token)
-    client: BoxClient = BoxClient(auth=auth)
+    client = make_auth()
     verify_file("K99/R221107A/2023_03_09/15_52_17")
