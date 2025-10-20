12e3056fd20457bb68f019ad22b518055e34d355
aathok-git <aathokmail@gmail.com>
2025-10-20 10:16:29 -0600

    More work on filedownloader, might want to add the box folder id to the csv files to make things easier

--- data/file_downloader.py
+++ data/file_downloader.py
@@ -1,5 +1,6 @@
 from box_credentials import dev_token, base_file_path
 from box_sdk_gen import BoxClient, BoxDeveloperTokenAuth
+from os import path
 
 def main(token: str):
     auth: BoxDeveloperTokenAuth = BoxDeveloperTokenAuth(token=token)
@@ -8,18 +9,34 @@ def main(token: str):
         print(item.name)
     
 
-def verify_file(path):
+def verify_file(path: str):
     """Checks the path where the file should be.
     If a folder doesn't exist, break the search and call download_file(path) to download and store the file
     """
+    if not path.exists(f"{base_file_path}/{path}"):
+        file_id = get_file_id(path)
+        download_file(path, file_id)
+    elif path.exists(f"{base_file_path}/{path}"):
+        return True
+    else:
+        print("Error (error message here)")
+
+def get_file_id(path: str, csv="experiments.csv"):
+    """Opens and reads the CSV file (experiements.csv by default) to get the box id of that experiment folder"""
     pass
 
-def download_file(path):
+
+def download_file(path: str, ID):
     """Called from verify_file or run manually to download a file from box and store it in a standard path
     """
     
-    #Auth block here
+    auth: BoxDeveloperTokenAuth = BoxDeveloperTokenAuth(token=dev_token)
+    client: BoxClient = BoxClient(auth=auth)
     
+    try:
+        pass
+    except Exception as e:
+        pass
     #Try to download, if error then raise an appropriate error
     
     #Download path: f"{base_file_path}/{path}
@@ -27,14 +44,6 @@ def download_file(path):
     #Upon success return true
     
     pass
-
-
-
-
-
-
-
-
 
 
 
