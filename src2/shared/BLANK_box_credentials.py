04e91f5615ae02be91eebcb0e6d9e6f018601832
aathok-git <aathokmail@gmail.com>
2025-12-04 11:04:11 -0700

    Final polish on blank box credentials

--- src2/shared/BLANK_box_credentials.py
+++ src2/shared/BLANK_box_credentials.py
@@ -2,8 +2,8 @@
 THIS PROGRAM WAS DEVELOPED FOR BOX-SDK-10.0.1
 
 To properly set up this file:
-1. Make a copy of this file and rename it to "box_credentials.py" 
-2. Insert your box credentials in the appropriate (For directions on how to obtain these credentials, see the documentation here: <https://developer.box.com/guides/authentication/client-credentials/client-credentials-setup/>)
+1. *Make a copy of this file and rename it to "box_credentials.py"* 
+2. Insert your box credentials in the appropriate fields (For directions on how to obtain these credentials, see the documentation here: <https://developer.box.com/guides/authentication/client-credentials/client-credentials-setup/>)
     a. To obtain your box credentials, go to the box developer console at <https://byu.app.box.com/developers/console>
     b. If you're the first person on your team to set this up, create a custom app and select "Server Authentication (Client Credentials Grant)". This app can accommodate up to 15 team members.
       a. Navigate to configuration and select the box labeled "Write all files and folders stored in Box"
